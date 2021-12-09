1330216150257bc61b7146f3af80039276966a07
hwat212 <73551461+hwat212@users.noreply.github.com>
2021-12-09 11:52:47 -0500

    Add files via upload

--- Project 3 DSA REAL.py
+++ Project 3 DSA REAL.py
@@ -6,7 +6,8 @@ white = 255, 255, 255
 black = 0, 0, 0
 activeColor = pygame.Color('lightskyblue2')
 passiveColor = pygame.Color('gray30')
-red = pygame.Color('red')
+pretty = pygame.Color('cadetblue4')
+background = pygame.Color('grey8')
 sourceColor = passiveColor
 searchColor = passiveColor
 
@@ -17,6 +18,7 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Six Degrees of Steam Separation')
 base_font = pygame.font.SysFont('monospace', 32, True)
+title_font = pygame.font.Font(None, 70)
 user_text = ''
 search_text = ''
 textWidth, textHeight = base_font.size("a")
@@ -25,7 +27,14 @@ inputBox = pygame.Rect(250, 250, 400, 40)
 searchBox = pygame.Rect(250, 400, 400, 40)
 blackBox = pygame.Rect(-140, 250, 400, 40)
 blackBox2 = pygame.Rect(-140, 400, 400, 40)
-toSearch = pygame.Rect(800, 325, 100, 40)
+toSearch = pygame.Rect(800, 325, 21 *  textWidth + 22, 40)
+printBox = pygame.Rect(300, 550, 11 * textWidth + 22, 40)
+
+box1 = base_font.render('Search from:', True, white) 
+box2 = base_font.render('Search to:', True, white) 
+printHere = base_font.render('Print graph', True, white) 
+searchText = base_font.render('Search for connection', True, white) 
+title = title_font.render('Six Degrees of Steam Separation', True, white) 
 
 moveLeft1 = 0
 moveLeft2 = 0
@@ -36,6 +45,8 @@ def Search(source, search):
     print('Target: ', search)
 
 while 1:
+
+    backTouched = False
 
     for event in pygame.event.get():
 
@@ -70,6 +81,13 @@ while 1:
                 searchActive = False
                 searchColor = passiveColor
 
+            elif printBox.collidepoint(event.pos):
+
+                sourceActive = False
+                sourceColor = passiveColor
+                searchActive = False
+                searchColor = passiveColor
+
             else:
 
                 sourceActive = False
@@ -81,6 +99,7 @@ while 1:
 
             if event.key == pygame.K_BACKSPACE:
 
+                backTouched = True
                 user_text = user_text[0:-1]
 
                 if len(user_text) > 20:
@@ -91,6 +110,8 @@ while 1:
 
                     moveLeft1 = 0
 
+                clock.tick(7)
+                    
             elif event.key == pygame.K_RETURN:
 
                 Search(user_text, search_text)
@@ -113,6 +134,7 @@ while 1:
 
             if event.key == pygame.K_BACKSPACE:
 
+                backTouched = True
                 search_text = search_text[0:-1]
 
                 if len(search_text) > 20:
@@ -122,6 +144,8 @@ while 1:
                 else:
 
                     moveLeft2 = 0
+
+                clock.tick(7)
 
             elif event.key == pygame.K_RETURN:
 
@@ -139,25 +163,59 @@ while 1:
 
                 else:
 
-                    moveLeft2 = 0
+                    moveLeft2 = 0        
+    
+    keys = pygame.key.get_pressed()
 
-    screen.fill(black)
+    if keys[pygame.K_BACKSPACE] and backTouched == False:
+
+        if sourceActive:
+
+            user_text = user_text[0:-1]
+
+            if len(user_text) > 20:
+
+                moveLeft1 -= textWidth
+
+            else:
+
+                moveLeft1 = 0
+
+            clock.tick(7)
+
+        elif searchActive:
+
+            search_text = search_text[0:-1]
+
+            if len(search_text) > 20:
+
+                moveLeft2 -= textWidth
+
+            else:
+
+                moveLeft2 = 0
+
+            clock.tick(7)
+
+    screen.fill(background)
     text_surface = base_font.render(user_text, True, white)
     search_surface = base_font.render(search_text, True, white)
-    box1 = base_font.render('Search from:', True, white) 
-    box2 = base_font.render('Search to:', True, white) 
 
     screen.blit(text_surface, (inputBox.x + 11 - moveLeft1, inputBox.y + 2))
     screen.blit(search_surface, (searchBox.x + 11 - moveLeft2, searchBox.y + 2))
-
-    pygame.draw.rect(screen, black, blackBox)
+ 
+    pygame.draw.rect(screen, background, blackBox)
     pygame.draw.rect(screen, sourceColor, inputBox, 2)
-    pygame.draw.rect(screen, black, blackBox2)
+    pygame.draw.rect(screen, background, blackBox2)
     pygame.draw.rect(screen, searchColor, searchBox, 2)
-    pygame.draw.rect(screen, red, toSearch)
+    pygame.draw.rect(screen, pretty, toSearch)
+    pygame.draw.rect(screen, pretty, printBox)
 
     screen.blit(box1, (inputBox.x - 235, inputBox.y + 2))
     screen.blit(box2, (searchBox.x - 235 + 2 * textWidth, searchBox.y + 2))
+    screen.blit(printHere, (printBox.x + 11, printBox.y + 2))
+    screen.blit(title, (240, 40))
+    screen.blit(searchText, (toSearch.x + 11, toSearch.y + 2))
 
     pygame.display.flip()
     clock.tick(60)
