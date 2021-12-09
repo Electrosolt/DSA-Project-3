514473e2ab1a4397105f879ead0a23dad9e041a4
hwat212 <73551461+hwat212@users.noreply.github.com>
2021-12-09 17:16:23 -0500

    Add files via upload

--- Project 3 DSA REAL.py
+++ Project 3 DSA REAL.py
@@ -1,6 +1,161 @@
 import sys, pygame
+from collections import deque
+from collections import defaultdict
+import requests
+
 pygame.init()
 
+apiKey = "69769964D41B7AAA6C41B8C8D98538B2"
+
+# Steam account class
+class SteamAccount:
+
+    maxIndex = 0
+
+    def __init__(self, ID):
+        self.ID = ID
+        self.index = SteamAccount.maxIndex
+        SteamAccount.maxIndex += 1
+
+    def __eq__(self, other):
+        return self.ID == other.ID
+    
+    # Returns a list of the steam IDs of the SteamAccount's friends
+    def getFriendList(self):
+        req = requests.get(f"http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key={apiKey}&steamid={self.ID}&relationship=friend")
+
+        ids = []
+        numIDs = 0
+        for user in req.json()['friendslist']['friends']:
+            numIDs += 1
+            ids.append(user['steamid'])
+        print(f"Total IDs: {numIDs}")
+        return ids
+    
+    # Returns a list of usernames corresponding to the input steam ID list. Static Function.
+    def getAccountNames(steamIDs):
+        # The request can only handle up to 100 IDs at once.
+        # Solution: Slice the ID list into blocks of 100 IDs each
+        blocks = []
+        for i in range(0, len(steamIDs), 100):
+            blocks.append(steamIDs[i:i+100])
+        
+        names = []
+        numNames = 0
+        for steamIDList in blocks:
+            print(f"Block Length: {len(steamIDList)}")
+            steamIDstr = ",".join(steamIDList)
+            req = requests.get(f"http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key={apiKey}&steamids={steamIDstr}")
+
+            for user in req.json()['response']['players']:
+                numNames += 1
+                names.append(user['personaname'])
+
+        print(f"Total Names: {numNames}")
+        return names
+
+# Adjacency List Implementation
+class AdjListGraph:
+
+    def __init__(self, source):
+        self.source = source
+        self.graph = defaultdict(list)
+
+    # Returns the number of friend connections needed to get from the source to the searched account, and -1 if not possible in 3 connections
+    def findConnection(self, target, maxDegrees):
+        queue = deque([self.source])
+        visited = set((self.source))
+        pathLength = 0 # Length of path from source to target, if found.
+
+        while queue and not pathLength > maxDegrees:
+            pathLength += 1
+            for i in range(len(queue)):
+                current = queue.popleft()
+                for child in self.graph[current]:
+                    if child == target:
+                        return pathLength
+                    elif child not in visited:
+                        queue.append(child)
+                        visited.add(child)
+        return -1
+    
+    # Inserts an edge (a friendship connection, and possible vertex, an account) into the graph
+    def inserEdge(self, source, target):
+        self.graph[source].append(target)
+
+    # Prints all accounts in the graph
+    def printGraph(self):
+        queue = deque([self.source])
+        visited = set((self.source))
+        
+        while queue:
+            current = queue.popleft()
+            print(f"ID: {current}")
+
+            for child in self.graph[current]:
+                if child not in visited:
+                    visited.add(child)
+                    queue.append(child)
+
+
+class AdjMatrixGraph:
+
+    def __init__(self, source):
+        self.source = source
+        self.graph = []
+        self.indexToID = {}
+
+    def findConnection(self, target, maxDegrees):
+        queue = deque([self.source.index])
+        visited = set((self.source.index))
+        pathLength = 0 # Length of path from source to target, if found.
+
+        while queue and not pathLength > maxDegrees:
+            pathLength += 1
+            for i in range(len(queue)):
+                current = queue.popleft()
+                for index, exists in enumerate(self.graph[current]):
+                    if not exists:
+                        continue
+                    if self.indexToID[index] == target:
+                        return pathLength
+                    elif index not in visited:
+                        queue.append(index)
+                        visited.add(index)
+        return -1
+    
+    def insertEdge(self, source, target):
+        if len(self.graph) > max(source.index, target.index):
+            self.graph.extend([False] * max(source.index, target.index) - len(self.graph))
+        
+
+        if len(self.graph[source.index]) > target.index:
+            self.graph[source.index].extend([False] * target.index - len(self.graph[source.index]))
+        
+        self.graph[source.index][target.index] = True
+        
+        if len(self.graph[target.index]) > source.index:
+            self.graph[target.index].extend([False] * source.index - len(self.graph[target.index]))
+        
+        self.graph[target.index][source.index] = True
+
+        self.indexToID[source.index] = source.id
+        self.indexToID[target.index] = target.id
+
+    def printGraph(self):
+        queue = deque([self.source])
+        visited = set((self.source))
+        
+        while queue:
+            current = queue.popleft()
+            print(f"ID: {self.indexToID[current]}")
+
+            for index, exists in enumerate(self.graph[current]):
+                if index not in visited and exists:
+                    visited.add(index)
+                    queue.append(index)
+
+# UI
 # Create colors
 size = width, height = 1280, 720
 white = 255, 255, 255
@@ -11,6 +166,7 @@ pretty = pygame.Color('cadetblue4')
 background = pygame.Color('grey8')
 sourceColor = passiveColor
 searchColor = passiveColor
+searchBoxColor = passiveColor
 
 # Set values of UI variables
 sourceActive = False
@@ -26,20 +182,23 @@ textWidth, textHeight = base_font.size("a")
 moveLeft1 = 0
 moveLeft2 = 0
 running = True
+graphCreated = False
 
 # Create text boxes and buttons
 inputBox = pygame.Rect(250, 250, 400, 40)
 searchBox = pygame.Rect(250, 400, 400, 40)
 blackBox = pygame.Rect(-140, 250, 400, 40)
 blackBox2 = pygame.Rect(-140, 400, 400, 40)
-toSearch = pygame.Rect(800, 325, 21 *  textWidth + 22, 40)
+toSearch = pygame.Rect(740, 400, 21 *  textWidth + 22, 40)
 printBox = pygame.Rect(300, 550, 11 * textWidth + 22, 40)
+createBox = pygame.Rect(740, 250, 24 * textWidth + 22, 40)
 
 # Render permanent text
 box1 = base_font.render('Search from:', True, white) 
 box2 = base_font.render('Search to:', True, white) 
 printHere = base_font.render('Print graph', True, white) 
 searchText = base_font.render('Search for connection', True, white) 
+createText = base_font.render('Create graph with source', True, white) 
 title = title_font.render('Six Degrees of Steam Separation', True, white) 
 
 # Search for connection when enter is pressed or search button is clicked
@@ -47,6 +206,10 @@ def Search(source, search):
 
     print('Source: ', source)
     print('Target: ', search)
+
+def CreateGraph(source):
+
+    graphSource = SteamAccount()
 
 # Program loop
 while running:
@@ -87,10 +250,13 @@ while running:
             # ...on the search button
             elif toSearch.collidepoint(event.pos):
 
-                # Reset text boxes and search for connection
-                Search(user_text, search_text)
-                user_text = ''
-                search_text = ''
+                # Reset text boxes and search for connection, if a graph is created
+                if graphCreated:
+
+                    Search(user_text, search_text)
+                    user_text = ''
+                    search_text = ''
+
                 sourceActive = False
                 sourceColor = passiveColor
                 searchActive = False
@@ -104,6 +270,17 @@ while running:
                 sourceColor = passiveColor
                 searchActive = False
                 searchColor = passiveColor
+
+            # ...on the create button
+            elif createBox.collidepoint(event.pos):
+
+                # Create a graph using the source
+                sourceActive = False
+                sourceColor = passiveColor
+                searchActive = False
+                searchColor = passiveColor
+                CreateGraph(user_text)
+                graphCreated = True
 
             # ...on anything else
             else:
@@ -246,13 +423,18 @@ while running:
     screen.blit(text_surface, (inputBox.x + 11 - moveLeft1, inputBox.y + 2))
     screen.blit(search_surface, (searchBox.x + 11 - moveLeft2, searchBox.y + 2))
  
+    if graphCreated:
+
+        searchBoxColor = pretty
+
     # Draw all rectangles
     pygame.draw.rect(screen, background, blackBox)
     pygame.draw.rect(screen, sourceColor, inputBox, 2)
     pygame.draw.rect(screen, background, blackBox2)
     pygame.draw.rect(screen, searchColor, searchBox, 2)
-    pygame.draw.rect(screen, pretty, toSearch)
+    pygame.draw.rect(screen, searchBoxColor, toSearch)
     pygame.draw.rect(screen, pretty, printBox)
+    pygame.draw.rect(screen, pretty, createBox)
 
     # Draw all permanent text
     screen.blit(box1, (inputBox.x - 235, inputBox.y + 2))
@@ -260,6 +442,7 @@ while running:
     screen.blit(printHere, (printBox.x + 11, printBox.y + 2))
     screen.blit(title, (240, 40))
     screen.blit(searchText, (toSearch.x + 11, toSearch.y + 2))
+    screen.blit(createText, (createBox.x + 11, createBox.y + 2))
 
     # Complete the program loop
     pygame.display.flip()
